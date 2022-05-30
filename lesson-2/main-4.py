ed4fcfc66f2c213773b4433c078be5ebcf9b7595
Somfer <furmanets_s@mail.ru>
2022-05-30 12:04:49 +0300

    fix

--- lesson-2/main-4.py
+++ lesson-2/main-4.py
@@ -1,4 +1,4 @@
-def isGreater(str1, str2):
+def Creteria(str1, str2):
     minLength = min(len(str1),len(str2))
     for i in range(minLength):
         if str1[i] > str2[i]:
@@ -20,20 +20,16 @@ def sort(arr, f):
             if f(arr[j], arr[j + 1]):
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
-
-def maxNumberMadeConstructedList(arr):
-    sort(arr, isGreater)
-    arr.reverse()
-    str = ""
-    for i in arr:
-        str+=i
-    return str
-
-
 print("Введите количесво значений массива")
 n = int(input())
 arr=[]
 for i in range(n):
     str=input()
     arr.append(str)
-print(maxNumberMadeConstructedList(arr))
+sort(arr, Creteria)
+arr.reverse()
+str = ""
+for i in arr:
+    str+=i
+print(str)
+
